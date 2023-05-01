c68703adcf875ed75adfe34a8eb3963eb26d170e
Ani Chilingaryan <chilingaryan12@gmail.com>
2023-05-01 16:13:46 -0700

    add ajax-giphy-party

---
+++ ajax-giphy-party/app.js
@@ -0,0 +1,36 @@
+console.log("Let's get this party started!");
+
+let gifArea = $("#gif-area");
+let input = $("#gif-search");
+
+function addGif(res) {
+    let numResults = res.data.length;
+    if (numResults) {
+        let randomIdx = Math.floor(Math.random() * numResults);
+        let newCol = $("<div>");
+        let newGif = $("<img>", {src: res.data[randomIdx].images.original.url});
+        newCol.append(newGif);
+        gifArea.append(newCol);
+    }
+}
+
+
+$(".submit-button").on("click", async function (event) {
+    event.preventDefault();
+
+    let searchTerm = input.val();
+    input.val("");
+
+    const res = await axios.get('http://api.giphy.com/v1/gifs/search?', {
+        params: {
+            q: searchTerm,
+            api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+        }
+    });
+    console.log(res.data);
+    addGif(res);
+});
+
+$("#remove").on("click", function() {
+    gifArea.empty();
+});
