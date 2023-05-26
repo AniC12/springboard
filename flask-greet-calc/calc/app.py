90fdeb370cc5dbc398b0e8e857906d047b4252b5
Ani Chilingaryan <chilingaryan12@gmail.com>
2023-05-25 21:35:51 -0700

    add flask greet calc

---
+++ flask-greet-calc/calc/app.py
@@ -0,0 +1,61 @@
+# Put your app in here.
+
+from flask import Flask, request
+from operations import add, sub, div, mult
+
+app = Flask(__name__)
+
+
+@app.route("/add")
+def do_add():
+    """Add a and b"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+    
+    return f'<h1>{a} + {b} = {result}</h1>'
+
+
+@app.route("/sub")
+def do_sub():
+    """Subtract a and b"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+    
+    return f'<h1>{a} - {b} = {result}</h1>'
+
+
+@app.route("/mult")
+def do_mult():
+    """Multiply a and b"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+    
+    return f'<h1>{a} * {b} = {result}</h1>'
+
+
+@app.route("/div")
+def do_div():
+    """Divide a and b"""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+    
+    return f'<h1>{a} / {b} = {result}</h1>'
+
+operators = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div
+}
+@app.route("/math/<oper>")
+def do_math(oper):
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[oper](a, b)
+
+    return str(result)
