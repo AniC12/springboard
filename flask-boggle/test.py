fb4af02e0e97ed952d3f98544e330eb66ce0782f
Ani Chilingaryan <chilingaryan12@gmail.com>
2023-06-02 12:55:26 -0700

    add flask boggle tests

--- flask-boggle/test.py
+++ flask-boggle/test.py
@@ -11,4 +11,27 @@ class FlaskTests(TestCase):
         self.client = app.test_client()
         app.config['TESTING'] = True  
 
-    #def test_homepage(self):
+    def test_homepage(self):
+        """Make sure information is in the session and HTML is displayed"""
+
+        with self.client:
+            response = self.client.get('/')
+            self.assertIn('board', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertIsNone(session.get('nplays'))
+            self.assertIn(b'<button>Enter</button>', response.data)
+
+    def test_invalid_word(self):
+        """Test if word is in the dictionary"""
+
+        self.client.get('/')
+        response = self.client.get('/check-word?word=abbreviature')
+        self.assertEqual(response.json['result'], 'not-on-board')
+
+    def non_english_word(self):
+        """Test if word is on the board"""
+
+        self.client.get('/')
+        response = self.client.get(
+            '/check-word?word=barev')
+        self.assertEqual(response.json['result'], 'not-word')
