75d58dbf2704ecf436fe25553db7d6033dfbb01b
Ani Chilingaryan <chilingaryan12@gmail.com>
2023-06-04 23:52:30 -0700

    add prob divide conquer

---
+++ prob-divide-conquer/count-zeroes.js
@@ -0,0 +1,29 @@
+function countZeroes(array) {
+    let leftIdx = 0;
+    let rightIdx = array.length - 1;
+    let midIdx = Math.floor((leftIdx + rightIdx) / 2);
+
+    while (leftIdx < rightIdx) {
+        if (array[midIdx] === 0) {
+            rightIdx = midIdx - 1;
+            midIdx = Math.floor((leftIdx + rightIdx) / 2);
+        } else {
+            leftIdx = midIdx + 1;
+            midIdx = Math.floor((leftIdx + rightIdx) / 2);
+        }
+    }
+
+    if (array[midIdx] === 0) {
+        return array.length - midIdx;
+    }
+
+    return array.length - midIdx - 1;
+
+}
+
+console.log(countZeroes([1, 1, 1, 1, 0, 0]))
+console.log(countZeroes([1, 0, 0, 0, 0]))
+console.log(countZeroes([0, 0, 0]))
+console.log(countZeroes([1, 1, 1, 1]))
+
+module.exports = countZeroes
---
+++ prob-divide-conquer/find-floor.js
@@ -0,0 +1,9 @@
+function findFloor() {
+    let leftIdx = 0;
+    let rightIdx = array.length - 1;
+    let midIdx = Math.floor((leftIdx + rightIdx) / 2);
+
+    
+}
+
+module.exports = findFloor
